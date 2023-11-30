dc96024dc806cfcbf334e653e0bce02ed5b7865f
Esteban <150131639+Estebanx55@users.noreply.github.com>
2023-11-30 17:16:43 +0100

    ptit push

--- jour03/job06/index.php
+++ jour03/job06/index.php
@@ -1,3 +1,11 @@
 <?php
-$str = "Les choses que l'on possède finissent par nous posséder"
+$str = "Les choses que l'on possede finissent par nous posseder";
+$largeurtext = strlen($str);
+$rstr = ""; 
+
+for ($i=$largeurtext - 1; $i >= 0; $i--) { 
+    $rstr .= $str[$i];
+}
+
+echo $rstr;
 ?>
