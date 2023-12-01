d8e1b396e5ec8178c2be81aba636000859a76bba
Esteban <150131639+Estebanx55@users.noreply.github.com>
2023-12-01 11:15:59 +0100

    petit push

--- jour03/job06/index.php
+++ jour03/job06/index.php
@@ -1,9 +1,9 @@
 <?php
 $str = "Les choses que l'on possede finissent par nous posseder";
 $largeurtext = strlen($str);
-$rstr = ""; 
+$rstr = "";
 
-for ($i=$largeurtext - 1; $i >= 0; $i--) { 
+for ($i = $largeurtext - 1; $i >= 0; $i--) {
     $rstr .= $str[$i];
 }
 
