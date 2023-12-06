f02e32ddca3991ee55800df63fe80f8beac3f825
Esteban <esteban5.bare@gmail.com>
2023-12-06 16:07:15 +0100

    fffffffff

--- jour07/job07/index.php
+++ jour07/job07/index.php
@@ -21,14 +21,13 @@
         <input type="submit" name="valider " value="Valider" />
     </form>
     <?php
-    function gras($str)
-    {
+    function gras($str) {
         $gras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
         $i = 0;
-        while (isset($str[$i])) {
+        while(isset($str[$i])) {
             $char = $str[$i];
-            if (in_array($char, $gras)) {
-                echo '<b>' . $str . '</b>';
+            if(in_array($char, $gras)) {
+                echo '<b>'.$str.'</b>';
                 break;
             } else {
                 echo $str;
@@ -36,17 +35,48 @@
             }
         }
     }
+    function cesar($str, $decalage = 2) {
+        $result = '';
+        for($i = 0; $i < strlen($str); $i++) {
+            $char = $str[$i];
+            if(ctype_alpha($char)) {
+                $isUpper = ctype_upper($char);
+                $char = chr((ord($char) - ($isUpper ? ord('A') : ord('a')) + $decalage) % 26 + ($isUpper ? ord('A') : ord('a')));
+            }
+            $result .= $char;
+        }
+        return $result;
+    }
+
+    function plateforme($str) {
+        $largeurText = strlen($str);
+        $rstr = "";
+    
+        for ($i = $largeurText - 1; $i >= 0; $i--) {
+            if ($i >= 1 && $str[$i - 1] == 'm' && $str[$i] == 'e') {
+                $rstr .= '_';
+            }
+            $rstr .= $str[$i];
+        }
+        return strrev($rstr);
+    }
     $str = $_POST['Mot'];
 
-    if (isset($_POST["foction"])) {
-        if ($_POST["foction"] == "gras") {
+    if(isset($_POST["foction"])) {
+        if($_POST["foction"] == "gras") {
             echo gras($str);
         }
     }
 
-    if (isset($_POST["foction"])) {
-        if ($_POST["foction"] == "cesar") {
-            echo gras($str);
+    if(isset($_POST["foction"])) {
+        if($_POST["foction"] == "cesar") {
+            echo cesar($str);
+        }
+    }
+
+    if(isset($_POST["foction"])) {
+        if($_POST["foction"] == "plateforme") {
+            echo plateforme($str);
         }
     }
     ?>
